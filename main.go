586f7ec876d6ce3ffe0d1083f0b440730f97a1bd
Baran Demirbaş <77154643+barandemirbas@users.noreply.github.com>
2021-02-16 15:04:48 +0300

    Add Port Option

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/xml"
+	"flag"
 	"fmt"
 	"github.com/barandemirbas/open-with"
 	"github.com/dietsche/rfsnotify"
@@ -11,6 +12,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -37,36 +39,38 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	openwith.Browser("localhost", 8080)
-	log.Println(fmt.Sprintf("Server running on http://localhost%s", ":8080"))
+	port := flag.Int("p", 8080, "Set the port to serve")
+	flag.Parse()
+	openwith.Browser("localhost", *port)
+	log.Println(fmt.Sprintf("Server running on http://localhost:%d", *port))
 	http.HandleFunc("/", Server)
-	http.HandleFunc("/reload", Websocket)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/reload", Reload)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
 
 // Server function is the web server
 func Server(w http.ResponseWriter, r *http.Request) {
 	h := html{}
 	w.Header().Set("Cache-Control", "no-store, max-age=0")
-	file, err := ioutil.ReadFile("index.html")
-	if r.URL.Path != "/" {
-		if strings.Contains(r.URL.Path, ".css") || strings.Contains(r.URL.Path, ".js") {
-			http.ServeFile(w, r, r.URL.Path[1:])
-		} else {
+	if strings.Contains(r.URL.Path, ".css") == false && strings.Contains(r.URL.Path, ".js") == false {
+		file, err := ioutil.ReadFile("index.html")
+		if r.URL.Path != "/" {
 			file, err = ioutil.ReadFile(r.URL.Path[1:] + ".html")
 		}
+		err = xml.NewDecoder(bytes.NewBuffer(file)).Decode(&h)
+		h.Body.Content += "<script>\nvar socket = new WebSocket(\"ws://localhost:8080/reload\");\nsocket.onopen = function () {\nconsole.log(\"Status: Connected.\");\n};\nsocket.onmessage = function (e) {\nlocation.reload();\n};\n</script>\n"
+		fmt.Fprintf(w, "<html lang='"+h.Lang+"'><head>"+h.Head.Content+"</head>\n"+"<body class='"+h.Body.Class+"' id='"+h.Body.Id+"' style='"+h.Body.Style+"'>"+h.Body.Content+"</body>\n</html>")
+		if err != nil && err != io.EOF {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		http.ServeFile(w, r, r.URL.Path[1:])
 	}
-	err = xml.NewDecoder(bytes.NewBuffer(file)).Decode(&h)
-	if err != nil && err != io.EOF {
-		fmt.Println(err)
-		return
-	}
-	h.Body.Content += "<script>\nvar socket = new WebSocket(\"ws://localhost:8080/reload\");\nsocket.onopen = function () {\nconsole.log(\"Status: Connected.\");\n};\nsocket.onmessage = function (e) {\nlocation.reload();\n};\n</script>\n"
-	fmt.Fprintf(w, "<html lang='"+h.Lang+"'><head>"+h.Head.Content+"</head>\n"+"<body class='"+h.Body.Class+"' id='"+h.Body.Id+"' style='"+h.Body.Style+"'>"+h.Body.Content+"</body>\n</html>")
 }
 
-// Websocket function is reload the page with websockets
-func Websocket(w http.ResponseWriter, r *http.Request) {
+// Reload function is detect file changes and reload the page with websockets
+func Reload(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	watcher, _ := rfsnotify.NewWatcher()
 	err = watcher.AddRecursive(".")
